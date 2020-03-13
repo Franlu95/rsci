0a36ac194a30c60b0f2eaca4f5b36550052ab5d6
Franlu95 <franlu.gauto@gmail.com>
2020-03-13 01:44:06 -0300

    proyecto uno rsci

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>RSCI</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header class="cabecera">
+		<img src="img/1 RS - Logo Pantalla Principal.png" alt="logo" class="logo">
+		<nav class="menu">
+				<ul>
+					<li><a href="#">CONTACTO</a></li>
+					<li><img src="img/1 RS - Barra Separación Texto.png"></li>
+					<li><a href="#">PUNTOS DE COBRANZA</a></li>
+					<li><img src="img/1 RS - Barra Separación Texto.png"></li>
+					<li><a href="#">LOTEAMIENTOS</a></li>
+					<li><img src="img/1 RS - Barra Separación Texto.png"></li>
+					<li><a href="#marcadorinmuebles">INMUEBLES</a></li>
+					<li><img src="img/1 RS - Barra Separación Texto.png"></li>
+					<li><a href="#marcadorservicio">SERVICIOS</a></li>
+					<li><img src="img/1 RS - Barra Separación Texto.png"></li>
+					<li><a href="#">EMPRESA</a></li>
+				</ul>
+			</nav>
+		<div class="idiomas">
+				<img class="spa" src="img/1 L RS - SPA.png">
+				<img class="ale" src="img/1 L RS - ALE.png">
+				<img class="eng" src="img/1 L RS - ENG.png">
+			</div>
+	</header>
+	
+	<main class="main">
+		<section class="slider">
+		</section>
+		
+		<section class="servicios">
+			<div class="contenido">
+				<a name="marcadorservicio"></a>
+				<div class="titulo"><img src="img/2 RS - Servicios Headline.png"></img></div>
+				<div class="botones">
+					<table>
+						<tbody>
+							<tr>
+								<td><img src="img/2 RS - ICO 1.png"></td>
+								<td><img src="img/2 RS - ICO 2.png"></td>
+								<td><img src="img/2 RS - ICO 3.png"></td>
+							</tr>
+							<tr>
+								<td>NEGOCIOS</td>
+								<td>INVERSIÓN</td>
+								<td>CONSULTORÍA</td>
+							</tr>
+							<tr>
+								<td>INMOVILIARIOS</td>
+								<td>FORESTAL</td>
+								<td>CONTABLE</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</section>
+		
+		<section class="inmuebles">
+			<div class="contenido">
+				<a name="marcadorinmuebles"></a>
+				<div class="titulo"><img src="img/3 RS - Inmuebles Headline.png"></img></div>
+				<div class="botones">
+					<table>
+						<tbody>
+							<tr>
+								<td><img src="img/3 RS - ICO 1.png"></td>
+								<td><img src="img/3 RS - ICO 2.png"></td>
+							</tr>
+							<tr>
+								<td>PROPIEDADES</td>
+								<td>PROPIEDADES</td>
+							</tr>
+							<tr>
+								<td>URBANAS</td>
+								<td>RURALES</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</section>
+		
+	</main>
+	
+	
+	<footer class="piedepagina"></footer>
+</body>
+</html>
